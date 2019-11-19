ce40d02430e5b9f83da17a18fa6af7458f86f87f
MR-Stan <michaelrstanley87@gmail.com>
2019-11-19 16:43:20 -0600

    updated letter constuctor to class, added play and findWord functions to index

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const Word = require("./word.js");
 const inquirer = require("inquirer");
 
 // once a choice is made, push to chosen
-let chosen = [];
+let chosenWords = [];
+let currentWord = "";
 
-let words = [
+let wordsArr = [
     "fish", "dog", "cat", "antelope", "squirrel",
     "opossum", "turkey", "chicken", "cow", "beaver",
     "pizza", "tacos", "asparagus", "steak", "shrimp",
@@ -13,28 +14,36 @@ let words = [
 
 // first run
 initialize() => {
-    inquirer.prompt([
-        {
-            name: "welcome",
-            type: "list",
-            message: "Welcome! To play, you must first answer a couple of questions. Will you?",
-            choices: ["Yes", "No"]
-        },
-        {
-            name: "welcome",
-            type: "list",
-            message: "Welcome! To play, you must first answer a couple of questions. Will you?",
-            choices: ["Yes", "No"]
-        }
-    ])
-        .then(data => {
-
-        })
+    console.log("Welcome! Press any letter key to being.");
+    play();
 }
 
 // independent of times played
 play() => {
+    if (chosenWords.length < wordsArr.length) {
+        currentWord = findWord();
+    }
+    else {
+        console.log("You've guessed all of the words!")
+        playAgain();
+    }
+}
 
+// finds word that hasn't been picked yet
+findWord() => {
+    // picks random word in words array
+    let newWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
+    // if the randomly selected word hasn't been chosen already
+    if (!chosenWords.includes(newWord)) {
+        // add the word to chosen words
+        chosenWords.push(newWord);
+        // current word is updated to newWord
+        return newWord;
+    }
+    else {
+        // try again
+        return findWord();
+    }
 }
 
 playAgain() => {
--- letter.js
+++ letter.js
@@ -1,24 +1,26 @@
 // constructor
-function Letter(letter) {
-    // stores character
-    this.letter = letter;
-    // stores whether or not this letter has been guessed
-    this.isGuessed = false;
-    // returns underlying character if letter has been guessed
-    // or underscore if not
-    this.getCharacter = () => {
-        if (this.isGuessed) {
-            return this.letter;
+class Letter {
+    constructor(letter) {
+        // stores character
+        this.letter = letter;
+        // stores whether or not this letter has been guessed
+        this.isGuessed = false;
+        // returns underlying character if letter has been guessed
+        // or underscore if not
+        this.getCharacter = () => {
+            if (this.isGuessed) {
+                return this.letter;
+            }
+            else {
+                return "_";
+            }
         }
-        else {
-            return "_";
-        }
-    }
-    // takes character as argument and checks it against the 
-    // underlying character, updating the boolean if correct
-    this.checkCharacter = (char) => {
-        if (char === this.letter) {
-            this.isGuessed = true;
+        // takes character as argument and checks it against the 
+        // underlying character, updating the boolean if correct
+        this.checkCharacter = (char) => {
+            if (char === this.letter) {
+                this.isGuessed = true;
+            }
         }
     }
 }
